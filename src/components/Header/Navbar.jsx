117b0cc6767d2db7d5b15c62d9300c60f9ac5165
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-03 13:12:27 +0600

    updated navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="sticky top-0 z-50 bg-gray-400/50 backdrop-blur-lg shadow-md">
+            <div className="sticky top-0 z-50 bg-gray-500/50 backdrop-blur-lg shadow-md">
                 <div className="navbar max-w-7xl mx-auto px-4 py-2">
                     <div className="navbar-start flex items-center">
                         <Link to="/" className="text-2xl font-bold text-white tracking-wide">
